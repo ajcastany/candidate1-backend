ea235882b1964be60d1c931109a62259b9f72378
wolfeius <ajcastany@gmail.com>
2022-06-01 14:34:22 +0100

    ADD: config parser

--- main.py
+++ main.py
@@ -1,2 +1,26 @@
 #!/usr/bin/python3
+import configparser
+import json
+from typing import Dict, List
 
+
+def read_config(file_path):
+    dictionary = dict()
+    dict_list = list()
+    config = configparser.ConfigParser()
+    config.read(file_path)
+    for section in config.sections():
+        for key in config[section]:
+            dictionary[key] = config[section][key]
+    return (dictionary)
+
+
+print(read_config("db_conn.config.example"))
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
